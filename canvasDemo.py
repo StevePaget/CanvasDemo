683eebdfe828fe6df90e115490aa2e9f324e5f6c
Steve Paget <pagetworld@gmail.com>
2020-09-30 10:01:47 +0100

    Fixed image draw

--- canvasDemo.py
+++ canvasDemo.py
@@ -1,31 +1,34 @@
-import tkinter as tk
-from tkinter import font as tkFont
-from winsound import *
+from tkinter import *
+import tkinter.font as tkFont
+import tkinter.scrolledtext as scrolledtext
+import random
 
 
-class App(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
+
+class App:
+    def __init__(self, master):
+        self.master = master
         self.titlefont = tkFont.Font(family="Arial", size=20, slant="italic")
         self.buttonfont = tkFont.Font(family="Arial", size=18)
-        self.title("Canvas Demo")
         
-        lab1 = tk.Label(self, text="Canvas demo", font=self.titlefont)
-        lab1.grid(row=0, column=0, sticky=tk.W)
-        self.theCanvas =tk.Canvas(self, width=650,height = 600, bg="#fffed6")
+        lab1 = Label(master, text="Canvas demo", font=self.titlefont)
+        lab1.grid(row=0, column=0, sticky=W)
+        self.theCanvas =Canvas(master, width=650,height = 600, bg="#fffed6")
         self.theCanvas.grid(row=1, column=0, rowspan=6)
         
-        b1 = tk.Button(self, text= "Draw Shapes", font=self.buttonfont, command=self.drawShapes)
+        b1 = Button(master, text= "Draw Shapes", font=self.buttonfont, command=self.drawShapes)
         b1.grid(row=1, column=1, sticky="EW")
-        b2 = tk.Button(self, text= "Draw Pictures", font=self.buttonfont, command=self.drawImages)
+        b2 = Button(master, text= "Draw Pictures", font=self.buttonfont, command=self.drawImages)
         b2.grid(row=2, column=1, sticky="EW")
-        b3 = tk.Button(self, text= "Animate", font=self.buttonfont)
+        b3 = Button(master, text= "Animate", font=self.buttonfont)
         b3.grid(row=3, column=1, sticky="EW")
         
-        self.rowconfigure(4,weight=1)
+        master.rowconfigure(4,weight=1)
+        
         
     def drawShapes(self):
         # this will clear the canvas and draw a series of shapes and text
+        self.theCanvas.delete(ALL)
         # the shape positions are not remembered, so it is not possible to erase or move them
         
         # The reference for these commands can be found at effbot.org/tkinterbook/canvas.htm
@@ -46,10 +49,16 @@ class App(tk.Tk):
         
         
     def drawImages(self):
-        self.theCanvas.delete(tk.ALL)
-        jeffPic = tk.PhotoImage(file="sliderLeft.png")
-        self.theCanvas.create_image(50, 50, image=jeffPic, anchor="nw")
+        self.theCanvas.delete(ALL)
+        self.jeff = PhotoImage(file="jeff.png")
+        for _ in range(5):
+            self.theCanvas.create_image(random.randint(0,500),random.randint(0,500),image=self.jeff, anchor=NW)
         
 
 
-window = App()
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Canvas Demo")
+    root.resizable(width=False, height=False)
+    app = App(root)
+    root.mainloop()
