60b6b39a18d5ba05e57b9510ad9839e1dca1553f
stevepaget <pagetworld@gmail.com>
2021-09-28 11:03:33 +0100

    added skeleton

---
+++ canvasskeleton.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import tkinter.font as tkFont
+    
+
+class App(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.titlefont = tkFont.Font(family="Arial", size=20, slant="italic")
+        self.buttonfont = tkFont.Font(family="Arial", size=18)
+        
+
+if __name__ == "__main__":
+    app = App()
